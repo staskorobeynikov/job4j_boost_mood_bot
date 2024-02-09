413221d2dcc1afa981c2bb38f568b2aab5f9b1b7
staskorobejnikov <stas.korobeinikov@mail.ru>
2024-02-09 12:00:00 +0700

    DI, IoC, Beans, BeanFactory, ApplicationContext [#505289]

--- src/main/java/ru/job4j/service/BotCommandHandler.java
+++ src/main/java/ru/job4j/service/BotCommandHandler.java
@@ -1,4 +1,11 @@
 package ru.job4j.service;
 
+import org.springframework.stereotype.Service;
+import ru.job4j.content.Content;
+
+@Service
 public class BotCommandHandler {
+    void receive(Content content) {
+        System.out.println(content);
+    }
 }
