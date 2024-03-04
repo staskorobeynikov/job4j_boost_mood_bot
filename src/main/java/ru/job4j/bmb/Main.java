2204f1b0a1a8ec5bfeda0204c94173d3d517ae56
staskorobejnikov <stas.korobeinikov@mail.ru>
2024-03-04 12:00:00 +0700

    Настройка окружения *.properties [#505377]

--- src/main/java/ru/job4j/bmb/Main.java
+++ src/main/java/ru/job4j/bmb/Main.java
@@ -30,4 +30,11 @@ public class Main {
             appConfig.printConfig();
         };
     }
+
+    @Bean
+    public CommandLineRunner checkEnv(ApplicationContext ctx) {
+        return args -> {
+            System.out.println(ctx.getEnvironment().getProperty("telegram.bot.name"));
+        };
+    }
 }
