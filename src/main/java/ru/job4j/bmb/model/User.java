e9101a2d9dc74c378282666230d0e43b390da129
staskorobejnikov <stas.korobeinikov@mail.ru>
2024-05-25 13:20:00 +0700

    Spring Data JPA [#505298]

--- src/main/java/ru/job4j/bmb/model/User.java
+++ src/main/java/ru/job4j/bmb/model/User.java
@@ -1,10 +1,25 @@
 package ru.job4j.bmb.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.util.Objects;
 
+@Entity
+@Table(name = "mb_user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "client_id")
     private long clientId;
+
+    @Column(name = "chat_id")
     private long chatId;
 
     public User() {
@@ -49,13 +64,11 @@ public class User {
             return false;
         }
         User user = (User) o;
-        return clientId == user.clientId
-               && chatId == user.chatId
-               && Objects.equals(id, user.id);
+        return Objects.equals(id, user.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, clientId, chatId);
+        return Objects.hash(id);
     }
 }
--- src/main/java/ru/job4j/bmb/repository/UserRepository.java
+++ src/main/java/ru/job4j/bmb/repository/UserRepository.java
@@ -1,13 +1,14 @@
 package ru.job4j.bmb.repository;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import ru.job4j.bmb.model.User;
 
 import java.util.List;
 
-public interface UserRepository {
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
     List<User> findAll();
 
     User findByClientId(Long clientId);
-
-    void save(User user);
 }
